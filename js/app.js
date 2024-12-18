e61c2f756ccb2301d21f9ccaa40b652c69a4cdb5
STORMTR00P3R <noorblum16@gmail.com>
2024-12-17 17:06:54 -0800

    puhs

--- js/app.js
+++ js/app.js
@@ -7,77 +7,78 @@ const statesDropdown = document.getElementById("states");
 let num = 0;
 
 function generateDropdown() {
-  fetch("./states.json")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((stateData) => {
-        const opt = document.createElement("option");
-        opt.value = stateData.state;
-        opt.textContent = stateData.state;
-        statesDropdown.append(opt);
-      });
-    });
+    fetch("./states.json")
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((stateData) => {
+                const opt = document.createElement("option");
+                opt.value = stateData.state;
+                opt.textContent = stateData.state;
+                statesDropdown.append(opt);
+            });
+        });
 }
 
 generateDropdown();
 
 const account = new Account();
 
-balanceLabel.textContent = account._balance;    
+balanceLabel.textContent = account._balance;
 
 addBtn.addEventListener("click", () => {
-  // CHANGED: Prevent submission with empty input value
+    // CHANGED: Prevent submission with empty input value
 
-  if (amountInput.value === "") {
-    alert("Empty value. Please enter a valid amount.");
-  }
+    if (amountInput.value === "") {
+        alert("Empty value. Please enter a valid amount.");
+    }
 
-  let amount = parseFloat(amountInput.value);
+    let amount = parseFloat(amountInput.value);
 
-  let type = amountType.value;
-  let item = document.createElement("li");
+    let type = amountType.value;
+    let item = document.createElement("li");
 
-  // FIXME: Expenses label is recreated on every click
-  let expObj = document.createElement("p");
-  expObj.textContent = "Expenses";
-  let prefix = "";
-  let exp = document.getElementById("exp");
+    // FIXME: Expenses label is recreated on every click
+    let expObj = document.createElement("p");
+    expObj.textContent = "Expenses";
+    let prefix = "";
+    let exp = document.getElementById("exp");
 
-  if (num == 0) {
-    num++;
-  } else if (num == 1) {
-    num = 2;
-  }
+    if (num == 0) {
+        num++;
+    } else if (num == 1) {
+        num = 2;
+    }
 
-  if (num == 1) {
-    exp.appendChild(expObj);
-  }
+    if (num == 1) {
+        exp.appendChild(expObj);
+    }
 
-  if (type == "loss") {
-    account.updateBalance(amount, type);
-    item.style.color = "red";
-    prefix = "-$";
-  } else {
-    account.updateBalance(amount, type);
-    item.style.color = "green";
-    prefix = "+$";
-  }
+    if (type == "loss") {
+        account.updateBalance(amount, type);
+        item.style.color = "red";
+        prefix = "-$";
+    } else {
+        account.updateBalance(amount, type);
+        item.style.color = "green";
+        prefix = "+$";
+    }
 
-  item.textContent = prefix + amount.toFixed(2);
-  expensesList.appendChild(item);
-  balanceLabel.textContent = account.balance.toFixed(2);
-  amountInput.value = "";
+    item.textContent = prefix + amount.toFixed(2);
+    expensesList.appendChild(item);
+    balanceLabel.textContent = account.balance.toFixed(2);
+    amountInput.value = "";
+    amountInput.focus();
 });
 
 statesDropdown.addEventListener("change", () => {
-  fetch("./states.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const selectedState = statesDropdown.value;
-      const stateData = data.find((dataObj) => dataObj.state === selectedState);
-      account.updateLocation(stateData);
-      balanceLabel.textContent = account.balance;
-    })
-    .catch((error) => console.error("Error fetching JSON:", error));
+    fetch("./states.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const selectedState = statesDropdown.value;
+            const stateData = data.find((dataObj) => dataObj.state === selectedState);
+            account.updateLocation(stateData);
+            balanceLabel.textContent = account.balance;
+        })
+        .catch((error) => console.error("Error fetching JSON:", error));
 });
 
--- js/comp.js
+++ js/comp.js
@@ -5,10 +5,69 @@ let exp = document.getElementById("newExp");
 let newBudget = document.getElementById("newB");
 const expensesList = document.getElementById("expenses-list");
 const expDropdown = document.getElementById("exp");
+let all = document.getElementById("total");
+let emoji = document.getElementById("emoji");
+let total = 0;
 
 let hExpenses = [];
 
+hExpenses = JSON.parse(localStorage.getItem("history"));
+
+if (hExpenses == null) {
+    hExpenses = [];
+}
+
+for (let i = 0; i < hExpenses.length; i++) {
+    let item = document.createElement("li")
+    item.textContent = "-$" + hExpenses[i].amount.toFixed(2) + " " + hExpenses[i].category;
+    item.style.color = "red";
+    expensesList.appendChild(item);
+}
+
+function totalExp() {
+    total = 0;
+    for (let i = 0; i < hExpenses.length; i++) {
+        total = total + hExpenses[i].amount;
+    }
+}
+
+totalExp();
+
 const account = new Account();
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = Math.round(total / localStorage.getItem("initialBal") * 100);
+    if (width <= 100) {
+        elem.style.width = width + '%';
+        elem.innerHTML = width * 1 + '%';
+    } else if (width > 100) {
+        elem.style.width = 100 + '%';
+        elem.innerHTML = width * 1 + '%'
+    }
+    if (width <= 20) {
+        emoji.textContent = "😁";
+    } else if (width > 20 && width <= 40) {
+        emoji.textContent = "😃";
+    } else if (width > 20 && width <= 40) {
+        emoji.textContent = "🙂";
+    } else if (width > 40 && width <= 60) {
+        emoji.textContent = "😐";
+    } else if (width > 60 && width <= 80) {
+        emoji.textContent = "😰";
+    } else if (width > 80 && width < 100) {
+        emoji.textContent = "😱";
+    } else if (width == 100) {
+        emoji.textContent = "🫡";
+    }
+}
+
+function reset() {
+    localStorage.removeItem("currentBal");
+    localStorage.removeItem("initialBal");
+    localStorage.removeItem("history");
+    window.location.reload();
+}
 
 function generateDropdown() {
     fetch("./exp.json")
@@ -26,41 +85,48 @@ function generateDropdown() {
 generateDropdown();
 
 expBtn.addEventListener("click", () => {
-    let item = document.createElement("li");
-    let amount = parseFloat(exp.value);
-    let prefix = "";
-    console.log(amount)
+    if (exp.value !== "") {
+        let item = document.createElement("li");
+        let amount = parseFloat(exp.value);
+        let prefix = "";
 
-    account.updateBalance(amount, "loss");
-    item.style.color = "red";
-    prefix = "-$";
-    localStorage.setItem("currentBal", account.balance);
+        account.updateBalance(amount, "loss");
+        item.style.color = "red";
+        prefix = "-$";
+        localStorage.setItem("currentBal", account.balance);
 
-    item.textContent = prefix + amount.toFixed(2) + " " + expDropdown.value;
-    expensesList.appendChild(item);
-    amountInput.value = "";
-    newBudget.textContent = account._balance;
+        item.textContent = prefix + amount.toFixed(2) + " " + expDropdown.value;
+        expensesList.appendChild(item);
+        amountInput.value = "";
+        newBudget.textContent = account._balance;
 
-    let newExpense = {
-        amount,
-        category: expDropdown.value
-    };
+        let newExpense = {
+            amount,
+            category: expDropdown.value
+        };
 
-    hExpenses.push(newExpense);
+        hExpenses.push(newExpense);
 
+        localStorage.setItem("history", JSON.stringify(hExpenses));
 
-    localStorage.setItem("history", JSON.stringify(hExpenses));
+        totalExp();
+
+        move();
+    }
 });
 
 
-
 changeBtn.addEventListener("click", () => {
-    let amount = parseFloat(amountInput.value);
-    account.balance = amount;
+    if (amountInput.value != "") {
+        let amount = parseFloat(amountInput.value);
+        account.balance = amount;
 
-    newBudget.textContent = account.balance;
+        newBudget.textContent = account.balance;
 
-    localStorage.setItem("currentBal", account.balance);
+        localStorage.setItem("currentBal", account.balance);
+
+        localStorage.setItem("initialBal", account.balance);
+    }
 });
 
 if (localStorage.getItem("currentBal") != "") {
