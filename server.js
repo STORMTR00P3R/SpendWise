8d5c6a9afe5dd0324f599e2589ae0936d6385b0c
STORMTR00P3R <noorblum16@gmail.com>
2025-01-24 18:32:30 -0800

    psuh

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("boom")
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require('path')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/index.html'));
+})
+
+app.get('/compound', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/comp.html'))
+})
+
+app.get('/resources', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/re.html'))
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
