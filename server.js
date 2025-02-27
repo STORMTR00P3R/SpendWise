1145452d1f11d28e0990dfd263ad2b2043314e47
STORMTR00P3R <noorblum16@gmail.com>
2025-02-26 16:01:04 -0800

    push

--- server.js
+++ server.js
@@ -12,34 +12,65 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 app.get('/api/states', (req, res) => {
-    res.send(statesData)
+    res.status(200).send(statesData)
 })
 
 app.get('/api/categories', (req, res) => {
-    res.send(cataData)
+    res.status(200).send(newData)
 })
 
 app.get('/api/expenses', (req, res) => {
-    res.send(arr)
+    res.status(200).send(expData)
+})
+
+app.get('/api/expense/:id', (req, res) => {
+    console.log("request.params object: ", req.params)
+    const id = Number(req.params.id)
+    let foundItem = expData.find(function (expenseData) {
+        if (expenseData.id === id) return true
+    })
+
+    console.log(foundItem)
+
+    if (!foundItem) {
+        res.status(404).send({ error: "Expense data not found" })
+    }
+
+    if (foundItem) {
+        res.status(200).send(foundItem)
+    }
 })
 
 app.post('/api/expense', (req, res) => {
     const newData = req.body
+    if (!newData.amount || !newData.category) {
+        res.status(400).send({ error: "Amount or category missing from request" })
+    }
     newData.id = arr[arr.length - 1].id + 1;
     arr.push(newData)
 
     fs.writeFile('exp.json', JSON.stringify(arr), (err) => {
         if (err) {
-            console.error("Error writing file:", err);
+            res.status(500).send({ error: "Error with server: cannot write to database" })
         } else {
-            console.log("Successfully wrote to data.json");
+            console.log("Successfully wrote to data.json")
         }
     })
-    res.send(arr);
+    res.status(201).send(arr)
 })
 
 app.put('/api/expense', (req, res) => {
     let updatedItem = req.body
+    if (!updatedItem.amount || !updatedItem.category || !updatedItem.id) {
+        res.status(400).send({ error: "Amount or category or id missing from request" })
+    }
+    let foundItem = expData.find(function (expenseData) {
+        if (expenseData.id === req.body.id) return true
+    })
+
+    if (!foundItem) {
+        res.status(404).send({ error: "Expense data not found" })
+    }
 
     const newData = expData.map((expense, index) => {
         if (expense.id === updatedItem.id) {
@@ -50,16 +81,27 @@ app.put('/api/expense', (req, res) => {
     })
     fs.writeFile('exp.json', JSON.stringify(newData), (err) => {
         if (err) {
-            console.error("Error writing file:", err);
+            res.status(500).send({ error: "Error with server: cannot write to database" })
         } else {
             console.log("Successfully wrote to data.json");
         }
     })
 
-    res.send(newData);
+    res.status(200).send(newData)
 })
 
 app.delete('/api/expense', (req, res) => {
+    if (!req.body.id) {
+        res.status(400).send({ error: "ID missing from request" })
+    }
+    let foundItem = expData.find(function (expenseData) {
+        if (expenseData.id === req.body.id) return true
+    })
+
+    if (!foundItem) {
+        res.status(404).send({ error: "Expense data not found" })
+    }
+
     let newArr = arr.filter((expense) => {
         if (req.body.id != expense.id) {
             return true;
@@ -67,12 +109,12 @@ app.delete('/api/expense', (req, res) => {
     });
     fs.writeFile('exp.json', JSON.stringify(newArr), (err) => {
         if (err) {
-            console.error("Error writing file:", err);
+            res.status(500).send({ error: "Error with server: cannot write to database" })
         } else {
             console.log("Successfully wrote to data.json");
         }
     })
-    res.send(newArr);
+    res.status(200).send(expData)
 })
 
 app.listen(port, () => {
