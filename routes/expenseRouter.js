d9eccfd5dae948ba54d92097f33260e4debbebda
STORMTR00P3R <noorblum16@gmail.com>
2025-04-11 21:00:13 -0700

    mongo

--- routes/expenseRouter.js
+++ routes/expenseRouter.js
@@ -1,31 +1,11 @@
 import Expense from "../models/expenseSchema.js"
 import * as express from 'express'
-import expData from '../exp.json' with { type: "json" };
-import fs from 'fs'
 
 const expenseRouter = express.Router()
 
 expenseRouter.get('/', async (req, res) => {
     const result = await Expense.find()
     res.status(200).send(result)
-})
-
-expenseRouter.get('/:id', (req, res) => {
-    console.log("request.params object: ", req.params)
-    const id = Number(req.params.id)
-    let foundItem = expData.find(function (expenseData) {
-        if (expenseData.id === id) return true
-    })
-
-    console.log(foundItem)
-
-    if (!foundItem) {
-        res.status(404).send({ error: "Expense data not found" })
-    }
-
-    if (foundItem) {
-        res.status(200).send(foundItem)
-    }
 })
 
 expenseRouter.post('/', async (req, res) => {
@@ -41,58 +21,28 @@ expenseRouter.post('/', async (req, res) => {
     res.status(201).send(newExpense)
 })
 
-expenseRouter.put('/', (req, res) => {
+expenseRouter.put('/', async (req, res) => {
     let updatedItem = req.body
-    if (!updatedItem.amount || !updatedItem.category || !updatedItem.id) {
+    if (!updatedItem.amount || !updatedItem.category || !updatedItem._id) {
         res.status(400).send({ error: "Amount or category or id missing from request" })
     }
-    let foundItem = expData.find(function (expenseData) {
-        if (expenseData.id === req.body.id) return true
-    })
 
-    if (!foundItem) {
-        res.status(404).send({ error: "Expense data not found" })
-    }
+    let result = await Expense.findByIdAndUpdate(updatedItem._id, updatedItem, {new: true})
 
-    const newData = expData.map((expense, index) => {
-        if (expense.id === updatedItem.id) {
-            return updatedItem
-        } else {
-            return expense
-        }
-    })
-    fs.writeFile('exp.json', JSON.stringify(newData), (err) => {
-        if (err) {
-            res.status(500).send({ error: "Error with server: cannot write to database" })
-        }
-    })
-
-    res.status(200).send(newData)
+    res.status(200).send(result)
 })
 
-expenseRouter.delete('/', (req, res) => {
-    if (!req.body.id) {
+expenseRouter.delete('/', async (req, res) => {
+    if (!req.body._id) {
         res.status(400).send({ error: "ID missing from request" })
     }
-    let foundItem = expData.find(function (expenseData) {
-        if (expenseData.id === req.body.id) return true
-    })
 
-    if (!foundItem) {
-        res.status(404).send({ error: "Expense data not found" })
+    try {
+        let result = await Expense.findByIdAndDelete(req.body._id)
+        res.status(200).send(result)
+    } catch(err) {
+        res.status(500).send({message: "ID not found"})
     }
-
-    let newArr = expData.filter((expense) => {
-        if (req.body.id != expense.id) {
-            return true;
-        }
-    });
-    fs.writeFile('exp.json', JSON.stringify(newArr), (err) => {
-        if (err) {
-            res.status(500).send({ error: "Error with server: cannot write to database" })
-        }
-    })
-    res.status(200).send(expData)
 })
 
 export { expenseRouter }
